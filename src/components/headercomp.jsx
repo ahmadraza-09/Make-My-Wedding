cb088bddb8f78fa1b5355dab185c36a46d564569
Ahmad Raza <ahmadraza20082003@gmail.com>
2024-02-23 18:20:48 +0530

    Updated All Dropdown

--- src/components/headercomp.jsx
+++ src/components/headercomp.jsx
@@ -5,6 +5,11 @@ const Headercomp = () => {
 
   const [isVenueDropdownOpen, setVenueDropdownOpen] = useState(false);
   const [isVendorsDropdownOpen, setVendorsDropdownOpen] = useState(false);
+  const [isPhotosDropdownOpen, setPhotosDropdownOpen] = useState(false);
+  const [isRealWeddingDropdownOpen, setRealWeddingDropdownOpen] = useState(false);
+  const [isBlogDropdownOpen, setBlogDropdownOpen] = useState(false);
+  const [isShopDropdownOpen, setShopDropdownOpen] = useState(false);
+  const [isInviteDropdownOpen, setInviteDropdownOpen] = useState(false);
 
   const handleVenueDropdownToggle = () => {
     setVenueDropdownOpen(!isVenueDropdownOpen);
@@ -12,6 +17,26 @@ const Headercomp = () => {
 
   const handleVendorsDropdownToggle = () => {
     setVendorsDropdownOpen(!isVendorsDropdownOpen);
+  };
+
+  const handlePhotosDropdownToggle = () => {
+    setPhotosDropdownOpen(!isPhotosDropdownOpen);
+  };
+
+  const handleRealWeddingDropdownToggle = () => {
+    setRealWeddingDropdownOpen(!isRealWeddingDropdownOpen);
+  };
+
+  const handleBlogDropdownToggle = () => {
+    setBlogDropdownOpen(!isBlogDropdownOpen);
+  };
+
+  const handleShopDropdownToggle = () => {
+    setShopDropdownOpen(!isShopDropdownOpen);
+  };
+
+  const handleInviteDropdownToggle = () => {
+    setInviteDropdownOpen(!isInviteDropdownOpen);
   };
 
   const type1Data = {
@@ -57,18 +82,360 @@ const Headercomp = () => {
     'Cocktail Gowns',
     'Trousseau Sarees',
     'Bridal Lehenga on Rent',
-    'View All Bridal Wear',
-    'Groom Wear',
+    ],
+    link: 'View All Bridal Wear',
+  };
+
+  const type5Data = {
+    title: 'Jewellery & Accessories',
+    options: [
+    'Jewellery',
+    'Flower Jewellery',
+    'Bridal Jewellery on Rent',
+    'Accessories',
+    ],
+    link: 'View All Jewellery & Accessories',
+  };
+
+  const type6Data = {
+    title: 'Food',
+    options: [
+    'Catering Services',
+    'Cake',
+    'Chaat & Food Stalls',
+    'Bartenders',
+    'Home Catering',
+    ],
+    link: 'View All Food',
+  };
+
+  const type7Data = {
+    title: 'Makeup',
+    options: [
+    'Bridal Makeup',
+    'Family Makeup',
     ],
   };
 
-   const destinationData = [
+  const type8Data = {
+    title: 'Groom Wear',
+    options: [
+    'Sherwani',
+    'Wedding Suits / Tuxes',
+    'Sherwani On Rent',
+    ],
+    link: 'View All Groom Wear',
+  };
+
+  const type9Data = {
+    title: 'Invites & Gifts',
+    options: [
+    'Invitations',
+    'Favors',
+    'Trousseau Packers',
+    'Invitation Gifts',
+    'Mehndi Favors',
+    ],
+    link: 'View All Invites & Gifts',
+  };
+
+  const type10Data = {
+    title: 'Pandits',
+    options: [
+    'Wedding Pandits',
+    ],
+  };
+  
+  const type11Data = {
+    title: 'Pre Wedding Shoot',
+    options: [
+      'Pre Wedding Shoot Locations',
+      'Pre Wedding Photographers',
+    ],
+  };
+
+  const type12Data = {
+    title: 'Mehndi',
+    options: [
+    'Mehendi Artist',
+    ],
+  };
+
+  const type13Data = {
+    title: 'Music & Dance',
+    options: [
+    'DJs',
+    'Sangeet Choreographer',
+    'Wedding Entertainment',
+    ],
+  };
+
+  const type14Data = {
+    title: 'Bridal Grooming',
+    options: [
+    'Spa and Wellness',
+    'Beauty and Wellness',
+    ],
+  };
+
+  const type15Data = {
+    title: 'Planning & Decor',
+    options: [
+    'Wedding Planners',
+    'Decorators',
+    'Small Function Decor',
+    ],
+  };
+
+  const type16Data = {
+    title: 'Honeymoon',
+    options: [
+    'Honeymoon Packages',
+    ],
+  };
+
+  const type17Data = {
+    title: 'Outfit',
+    options: [
+    'Bridal Lehenga',
+    'Wedding Sarees',
+    'Engagement',
+    'Mehndi',
+    'Blouse Designs',
+    ],
+    link: 'More',
+  };
+
+  const type18Data = {
+    title: 'Decor & Ideas',
+    options: [
+    'Wedding Decor',
+    'Bridal Entry',
+    'Groom Entry',
+    'Wedding Games',
+    ],
+    link: 'More',
+  };
+
+  const type19Data = {
+    title: 'Groom Wear',
+    options: [
+    'Sherwani for Groom',
+    'Wedding Suits for Groom',
+    ],
+    link: 'More',
+  };
+
+  const type20Data = {
+    title: 'Jewellery & Accessories',
+    options: [
+    'Bridal Jewellery',
+    'Engagement Rings',
+    'Floral Jewellery',
+    ],
+    link: 'More',
+  };
+
+  const type21Data = {
+    title: 'Wedding Card Designs',
+    options: [
+    'Designs',
+    'Wedding Gifts',
+    'Wedding Invitations',
+    ],
+    link: 'More',
+  };
+
+  const type22Data = {
+    title: 'Bridal Makeup & Hair',
+    options: [
+    'Bridal Makeup',
+    'Bridal Hairstyles',
+    'Engagement',
+    'Mehndi',
+    ],
+    link: 'More',
+  };
+
+  const type23Data = {
+    title: 'Mehndi',
+    options: [
+    'Arabic',
+    'Mehndi Designs',
+    'Simple',
+    'Unique',
+    ],
+    link: 'More',
+  };
+
+  const type24Data = {
+    title: 'Wedding Photography',
+    options: [
+    'Pre Wedding Shoot',
+    'Wedding',
+    'Wedding Photoshoot & Poses',
+    ],
+    link: 'More',
+  };
+
+  const type25Data = {
+    title: 'By City',
+    options: [
+    'Mumbai',
+    'Pune',
+    'Banglore',
+    'Kolkata',
+    'Jaipur',
+    'Lucknow Mandapam',
+    'Hyderabad',
+    ],
+    link: 'Others',
+  };
+
+  const type26Data = {
+    title: 'By Culture',
+    options: [
+    'Maharashtrian',
+    'Punjabi /Sikh',
+    'Bengali',
+    'Gujarati',
+    'Marwari',
+    'Telugu',
+    ],
+    link: 'Others',
+  };
+
+  const type27Data = {
+    title: 'By Theme',
+    options: [
+    'Grand & Luxurious',
+    'Pocket Friendly Stunners',
+    'Intimate & Minimalist',
+    'Modern & Stylish',
+    'International',
+    ],
+    link: 'Others',
+  };
+
+  const type28Data = {
+    title: 'Browse by Category',
+    options: [
+    'Bridal Makeup',
+    'Honeymoon Travel',
+    'Wedding Songs and Videos',
+    'Bridal Hairstyles',
+    'Wedding Decor Ideas',
+    'Bridal Style Advice',
+    'Wedding Gifts and Favors',
+    'Budget Weddings',
+    ],
+    link: 'More',
+  };
+
+  const type29Data = {
+    title: 'Popular Sections',
+    options: [
+    'South Indian wedding',
+    'Real Brides Reveal',
+    'Bridal Buys',
+    'Real Wedding',
+    'Mehendi Ideas',
+    'Bridal Style Advice',
+    'Indian Wedding Ideas',
+    ],
+  };
+
+  const type30Data = {
+    title: 'Most Searched Blogs',
+    options: [
+    'Wedding Songs',
+    'Best Bridal Entry Songs',
+    'Chandni Chowk Lehengas',
+    'Best Groom Entry Songs',
+    'Wedding Decor Ideas',
+    'Bridal Mehendi Designs',
+    '2023 Marriage Dates',
+    'Wittiest Wedding Hashtags',
+    'Best Bridal Hairstyles',
+    'Latest Sabyasachi Lehengas',
+    'Bridal Blouse Designs',
+    ],
+  };
+
+  const type31Data = {
+    title: 'By Type',
+    options: [
+    'Bridal Lehenga',
+    'Lehenga',
+    'Sarees',
+    'Gowns',
+    'Sharara',
+    'Anarkali',
+    'Drape Sarees',
+    'Wittiest Wedding Hashtags',
+    'Indo Western',
+    ],
+    link: 'View More'
+  };
+
+  const type32Data = {
+    title: 'By Occasion',
+    options: [
+    'Engagement',
+    'Haldi',
+    'Mehendi',
+    'Cocktail',
+    'Wedding',
+    'Reception',
+    'Sangeet',
+    ],
+    link: 'View More'
+  };
+
+  const type33Data = {
+    title: 'Most Popular',
+    options: [
+    'Engagement lehenga',
+    'Cocktail Gowns',
+    'Wedding Sarees',
+    'Lehenga Drape Sarees',
+    'Silk Sarees',
+    'Banarasi Lehenga',
+    ],
+    link: 'View More'
+  };
+
+  const type34Data = {
+    title: 'Wedding Invitation Maker',
+    options: [
+    'Wedding Card Designs',
+    'Invitation Video Templates',
+    'Save the Date Templates',
+    ],
+  };
+
+  const destinationData = [
     { name: 'Goa', imageSrc: 'Images/goa.webp', altText: 'Goa' },
     { name: 'Jaipur', imageSrc: 'Images/jaipur.webp', altText: 'Jaipur' },
     { name: 'Udaipur', imageSrc: 'Images/udaipur.webp', altText: 'Udaipur' },
     { name: 'Jodhpur', imageSrc: 'Images/jodhpur.webp', altText: 'Jodhpur' },
     { name: 'Thailand', imageSrc: 'Images/thailand.webp', altText: 'Thailand' },
     { name: 'Jim-Corbett', imageSrc: 'Images/jim-corbett.webp', altText: 'Jim-Corbett' },
+  ];
+
+  const realWeddingData = [
+    { name: 'Shimul and Rahul (Goa)', imageSrc: 'Images/shimul-rahul-goa.webp', altText: 'Shimul and Rahul (Goa)' },
+    { name: 'Monica and Maninder (Greece)', imageSrc: 'Images/monica-maninder-greece.webp', altText: 'Monica and Maninder (Greece)' },
+  ];
+
+  const blogData = [
+    { name: 'Bridal Mehendi Design', imageSrc: 'Images/bridal-mehendi-design.webp', altText: 'Shimul and Rahul (Goa)' },
+    { name: 'Honeymoon Hotels', imageSrc: 'Images/honeymoon-hotels.webp', altText: 'Monica and Maninder (Greece)' },
+  ];
+
+  const shopData = [
+    { name: 'Bridal Mehendi Design', imageSrc: 'Images/bridal-mehendi-design.webp', altText: 'Shimul and Rahul (Goa)' },
+    { name: 'Honeymoon Hotels', imageSrc: 'Images/honeymoon-hotels.webp', altText: 'Monica and Maninder (Greece)' },
   ];
 
   return (
@@ -89,14 +456,83 @@ const Headercomp = () => {
                 <li onMouseEnter={handleVendorsDropdownToggle} onMouseLeave={handleVendorsDropdownToggle}>
                   <h3>Vendors</h3>
                   {isVendorsDropdownOpen && (
-                    <NavDropdown typelist3={type3Data} typelist4={type4Data} weddingDestination={destinationData} isDestinationDropdown={false} />
+                    <NavDropdown 
+                      typelist3={type3Data} 
+                      typelist4={type4Data} 
+                      typelist5={type5Data} 
+                      typelist6={type6Data} 
+                      typelist7={type7Data} 
+                      typelist8={type8Data} 
+                      typelist9={type9Data} 
+                      typelist10={type10Data} 
+                      typelist11={type11Data} 
+                      typelist12={type12Data} 
+                      typelist13={type13Data} 
+                      typelist14={type14Data} 
+                      typelist15={type15Data} 
+                      typelist16={type16Data} 
+                      weddingDestination={destinationData} 
+                      isDestinationDropdown={false} />
                   )}
                 </li>
-                <li>Photos</li>
-                <li>Real Weddings</li>
-                <li>Blog</li>
-                <li>Shop</li>
-                <li>E-Invites</li>
+                <li onMouseEnter={handlePhotosDropdownToggle} onMouseLeave={handlePhotosDropdownToggle}>
+                  <h3>Photos</h3>
+                  {isPhotosDropdownOpen && (
+                    <NavDropdown 
+                      typelist17={type17Data}
+                      typelist18={type18Data}
+                      typelist19={type19Data}
+                      typelist20={type20Data}
+                      typelist21={type21Data}
+                      typelist22={type22Data}
+                      typelist23={type23Data}
+                      typelist24={type24Data}
+                      weddingDestination={destinationData} 
+                      isDestinationDropdown={false} />
+                  )}
+                </li>
+                <li onMouseEnter={handleRealWeddingDropdownToggle} onMouseLeave={handleRealWeddingDropdownToggle}>
+                  <h3>Real Weddings</h3>
+                  {isRealWeddingDropdownOpen && (
+                    <NavDropdown 
+                      typelist25={type25Data}
+                      typelist26={type26Data}
+                      typelist27={type27Data}
+                      realWeddingDestination={realWeddingData} 
+                      isRealWeddingDropdown={true} />
+                  )}
+                </li>
+                <li onMouseEnter={handleBlogDropdownToggle} onMouseLeave={handleBlogDropdownToggle}>
+                  <h3>Blog</h3>
+                  {isBlogDropdownOpen && (
+                    <NavDropdown 
+                      typelist28={type28Data}
+                      typelist29={type29Data}
+                      typelist30={type30Data}
+                      blogDestination={blogData} 
+                      isBlogDropdown={true} />
+                  )}
+                </li>
+                <li onMouseEnter={handleShopDropdownToggle} onMouseLeave={handleShopDropdownToggle}>
+                  <h3>Shop</h3>
+                  {isShopDropdownOpen && (
+                    <NavDropdown 
+                      typelist31={type31Data}
+                      typelist32={type32Data}
+                      typelist33={type33Data}
+                      shopDestination={shopData} 
+                      isShopDropdown={true} />
+                  )}
+                </li>
+                <li onMouseEnter={handleInviteDropdownToggle} onMouseLeave={handleInviteDropdownToggle}>
+                  <h3>E-Invites</h3>
+                  {isInviteDropdownOpen && (
+                    <NavDropdown 
+                      typelist34={type34Data}
+                      inviteDestination={shopData} 
+                      isInviteDropdown={false} />
+                  )}
+                </li>
             </ul>
         </div>
         <div className="nav-right">
--- src/components/navdropdown.jsx
+++ src/components/navdropdown.jsx
@@ -2,16 +2,146 @@ import React from 'react';
 import TypeList from './typelist';
 import DestinationList from './destinationlist';
 
-const NavDropdown = ({ typelist1, typelist2, typelist3, typelist4, weddingDestination, isDestinationDropdown }) => {
+const NavDropdown = ({ 
+  typelist1, 
+  typelist2, 
+  typelist3, 
+  typelist4, 
+  typelist5, 
+  typelist6, 
+  typelist7, 
+  typelist8, 
+  typelist9, 
+  typelist10, 
+  typelist11, 
+  typelist12, 
+  typelist13, 
+  typelist14, 
+  typelist15, 
+  typelist16, 
+  typelist17, 
+  typelist18, 
+  typelist19, 
+  typelist20, 
+  typelist21, 
+  typelist22, 
+  typelist23, 
+  typelist24, 
+  typelist25, 
+  typelist26, 
+  typelist27, 
+  typelist28, 
+  typelist29, 
+  typelist30, 
+  typelist31, 
+  typelist32, 
+  typelist33, 
+  typelist34, 
+  weddingDestination, 
+  isDestinationDropdown,
+  realWeddingDestination,
+  isRealWeddingDropdown,
+  blogDestination,
+  isBlogDropdown,
+  shopDestination,
+  isShopDropdown,
+  inviteDestination,
+  isInviteDropdown }) => {
   return (
     <div className="dropdown">
       <div className="drop-content-section">
         {typelist1 && <TypeList title={typelist1.title} options={typelist1.options} link={typelist1.link}/>}
         {typelist2 && <TypeList title={typelist2.title} options={typelist2.options} link={typelist2.link}/>}
-        {typelist3 && <TypeList title={typelist3.title} options={typelist3.options} />}
-        {typelist4 && <TypeList title={typelist4.title} options={typelist4.options} />}
+        <div className="vendor-line">
+          {typelist3 && <TypeList title={typelist3.title} options={typelist3.options} />}
+          {typelist7 && <TypeList title={typelist7.title} options={typelist7.options} />}
+          {typelist11 && <TypeList title={typelist11.title} options={typelist11.options} />}
+          {typelist15 && <TypeList title={typelist15.title} options={typelist15.options} />}
+        </div>
+
+        <div className="vendor-line">
+          {typelist4 && <TypeList title={typelist4.title} options={typelist4.options} link={typelist4.link}/>}
+          {typelist8 && <TypeList title={typelist8.title} options={typelist8.options} link={typelist8.link}/>}
+          {typelist12 && <TypeList title={typelist12.title} options={typelist12.options} />}
+        </div>
+
+        <div className="vendor-line">
+          {typelist5 && <TypeList title={typelist5.title} options={typelist5.options} link={typelist5.link}/>}
+          {typelist9 && <TypeList title={typelist9.title} options={typelist9.options} link={typelist9.link}/>}
+          {typelist13 && <TypeList title={typelist13.title} options={typelist13.options} />}
+        </div>
+
+        <div className="vendor-line">
+          {typelist6 && <TypeList title={typelist6.title} options={typelist6.options} link={typelist6.link}/>}
+          {typelist10 && <TypeList title={typelist10.title} options={typelist10.options}/>}
+          {typelist14 && <TypeList title={typelist14.title} options={typelist14.options}/>}
+          {typelist16 && <TypeList title={typelist16.title} options={typelist16.options}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist17 && <TypeList title={typelist17.title} options={typelist17.options} link={typelist17.link}/>}
+          {typelist20 && <TypeList title={typelist20.title} options={typelist20.options} link={typelist20.link}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist18 && <TypeList title={typelist18.title} options={typelist18.options} link={typelist18.link}/>}
+          {typelist21 && <TypeList title={typelist21.title} options={typelist21.options} link={typelist21.link}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist19 && <TypeList title={typelist19.title} options={typelist19.options} link={typelist19.link}/>}
+          {typelist22 && <TypeList title={typelist22.title} options={typelist22.options} link={typelist22.link}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist23 && <TypeList title={typelist23.title} options={typelist23.options} link={typelist23.link}/>}
+          {typelist24 && <TypeList title={typelist24.title} options={typelist24.options} link={typelist24.link}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist25 && <TypeList title={typelist25.title} options={typelist25.options} link={typelist25.link}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist26 && <TypeList title={typelist26.title} options={typelist26.options} link={typelist26.link}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist27 && <TypeList title={typelist27.title} options={typelist27.options} link={typelist27.link}/>}
+        </div>
+        
+        <div className="vendor-line">
+          {typelist28 && <TypeList title={typelist28.title} options={typelist28.options} link={typelist28.link}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist29 && <TypeList title={typelist29.title} options={typelist29.options}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist30 && <TypeList title={typelist30.title} options={typelist30.options}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist31 && <TypeList title={typelist31.title} options={typelist31.options} link={typelist31.link}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist32 && <TypeList title={typelist32.title} options={typelist32.options} link={typelist32.link}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist33 && <TypeList title={typelist33.title} options={typelist33.options} link={typelist33.link}/>}
+        </div>
+
+        <div className="vendor-line">
+          {typelist34 && <TypeList title={typelist34.title} options={typelist34.options} />}
+        </div>
+
       </div>
       {isDestinationDropdown && <DestinationList title="Destination Wedding Venues" destinations={weddingDestination}/>}
+      {isRealWeddingDropdown && <DestinationList title="Latest Real Wedding" destinations={realWeddingDestination}/>}
+      {isBlogDropdown && <DestinationList title="Top Blogs" destinations={blogDestination}/>}
     </div>
   );
 };
